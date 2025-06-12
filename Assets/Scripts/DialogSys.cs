0e945b539c62620accbcfe631ab54cef62f0be8c
Alexander <cxz33335@gmail.com>
2025-06-12 23:08:05 +0300

    Система диалогов

---
+++ Assets/Scripts/DialogSys.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    public string[] lines;
+    public string name;
+    public Image image;
+    public float speedtext;
+    public Text dialoguetext;
+    public int index;
+
+    void Start()
+    {
+        dialoguetext.text = string.Empty;
+        index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    IEnumerator TypeLine()
+    {
+        foreach (char c in lines[index].ToCharArray())
+        {
+            dialoguetext.text += c;
+            yield return new WaitForSeconds(speedtext);
+        }
+    }
+
+    public void SkipNextClick()
+    {
+        if (dialoguetext.text == lines[index])
+        {
+            NextLine();
+        }
+        else
+        {
+            StopAllCoroutines();
+            dialoguetext.text = lines[index];
+        }
+    }
+
+    public void NextLine()
+    {
+        if (index < lines.Length - 1)
+        {
+            index++;
+            dialoguetext.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            SkipNextClick();
+        }
+    }
+}
