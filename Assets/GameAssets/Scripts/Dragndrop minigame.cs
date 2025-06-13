89111cec2f16399b78f28c1d604caa54efb9465a
sun1zu <kiri.andreev2006@yandex.ru>
2025-06-13 20:35:08 +0300

    fix comments

--- Assets/GameAssets/Scripts/Dragndrop minigame.cs
+++ Assets/GameAssets/Scripts/Dragndrop minigame.cs
@@ -97,7 +97,6 @@ public class Dragndropminigame : MonoBehaviour, IPointerDownHandler, IBeginDragH
             if (allSwordsmansInField&&allArchersInField)
             {
                 Debug.Log($"YOU WON GG!!!!!!!!!!");
-                //код победы хз что тут писать?тип тут игра должна продолжатся.
             }
         }
     }
@@ -106,13 +105,11 @@ public class Dragndropminigame : MonoBehaviour, IPointerDownHandler, IBeginDragH
         RectTransform rect1 = img1.rectTransform;
         RectTransform rect2 = img2.rectTransform;
 
-        // Получаем мировые координаты углов RectTransform
         Vector3[] corners1 = new Vector3[4];
         Vector3[] corners2 = new Vector3[4];
         rect1.GetWorldCorners(corners1);
         rect2.GetWorldCorners(corners2);
 
-        // Создаём Rect для каждого изображения
         Rect rectImg1 = new Rect(corners1[0].x, corners1[0].y,
                                  corners1[2].x - corners1[0].x,
                                  corners1[2].y - corners1[0].y);
